3c88eaab1cccd0d043cf33704f412613221e3686
Igor Sokorin <igorsokorin@MacBook-Pro-Igor.local>
2019-12-23 00:30:51 +0300

    pzdc

---
+++ hw — копия.py
@@ -0,0 +1,5 @@
+a_list = ['Email:', 'SSN:','Address:','Home Phone:','Mobile Phone: ','DOB:','Date of Surgery:','Date of Service:','Facility of Service:','Clinic Number:','Employer:','Work Phone: ','Fax: ','Type:','IPA:','Health Plan:','ID #:','Claims Address:','Group #:','Claim # / PO #:','Phone:','Fax:','Contact','Adjuster Email','Util Review Phone','Util Review Fax','Doctor:','NPI #: ','Date of Injury: ','Body Parts:','Body Part Side:','Gender:','Diagnosis:','Diagnosis 2:','Procedure:']
+a_list[0]
+print(a_list[0])
+a_list[-1]
+print(a_list[-1])
